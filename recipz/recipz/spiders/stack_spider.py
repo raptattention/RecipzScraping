5bacafa4c2f3452bc64353afefc34e00a79e19cc
raptattention <klaw1794@gmail.com>
2015-09-30 21:12:26 +0800

    Initial framework for scraping

---
+++ recipz/recipz/spiders/stack_spider.py
@@ -0,0 +1,9 @@
+from scrapy import Spider
+
+
+class StackSpider(Spider):
+    name = "stack"
+    allowed_domains = ["stackoverflow.com"]
+    start_urls = [
+        "http://stackoverflow.com/questions?pagesize=50&sort=newest",
+    ]
